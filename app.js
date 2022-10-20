8510ba820b0456aeac6869d23f53bfb528174204
LFC6times <72238524+LFC6times@users.noreply.github.com>
2022-10-19 23:39:06 -0700

    drag and drop only took 2 hours

--- app.js
+++ app.js
@@ -29,6 +29,11 @@ function putImageOnPage(image) {
     }
     img.src = "data:image/jpeg;base64," + image;
     document.getElementById("image").image = img;
+
+    setTimeout(() => {
+        document.getElementById("overlay_wrapper").style.height = document.getElementById("image").height + "px";
+        document.getElementById("overlay_wrapper").style.width = document.getElementById("image").width + "px"; // Needed so the overlaid div extends properly.
+    }, 0); // this made the code work??? i assume setting the element isn't instant and waiting for next event cycle via timeout fixes :P
 }
 
 function getImage() {
@@ -48,7 +53,8 @@ function getImage() {
             putImageOnPage(data.img);
             showUserMessage("You have clicked 0 times.");
 
-            document.getElementById("instructions").innerHTML = "Find all blue colored balls. Click on a top left corner and a bottom right corner that would enclose the ball. If there is no ball, click on the checkbox below. Hit 'Send' when done.";
+            // The below line remains for future use, where the server can send specific instructions along with the image.
+            document.getElementById("instructions").innerHTML = "Find all blue colored balls. Click on a top left corner and a bottom right corner that would enclose the ball. After clicking on 2 points, you can drag either point (green) to increase precision. If there is no ball, click on the checkbox below. Hit 'Send' when done.";
         }, (error) => {
             showUserMessage("Error getting image from the server: " + error);
         });
@@ -67,9 +73,37 @@ function postData(data) {
     });
 }
 
+function boundingBoxDrawer(x1, y1, x2, y2) {
+    setImageVisibility("line_1", true);
+    setImageVisibility("line_2", true);
+    setImageVisibility("line_3", true);
+    setImageVisibility("line_4", true);
+
+    document.getElementById("line_1").style.left = x1 + "px";
+    document.getElementById("line_1").style.top = y1 + "px";
+    document.getElementById("line_1").style.width = Math.abs(x2 - x1) + "px";
+    document.getElementById("line_1").style.transform = "rotate(0deg)";
+
+    document.getElementById("line_2").style.left = (x1 + 8) + "px";
+    document.getElementById("line_2").style.top = y1 + "px";
+    document.getElementById("line_2").style.width = Math.abs(y1 - y2) + "px";
+    document.getElementById("line_2").style.transform = "rotate(90deg)";
+
+    document.getElementById("line_3").style.left = x2 + "px";
+    document.getElementById("line_3").style.top = y2 + "px";
+    document.getElementById("line_3").style.width = Math.abs(x2 - x1) + "px";
+    document.getElementById("line_3").style.transform = "rotate(180deg)";
+
+    document.getElementById("line_4").style.left = x2 + "px";
+    document.getElementById("line_4").style.top = y2 + "px";
+    document.getElementById("line_4").style.width = Math.abs(y1 - y2) + "px";
+    document.getElementById("line_4").style.transform = "rotate(-90deg)";
+}
+
 function clickHandler(event) {
+    console.log("x: " + event.offsetX + ", y: " + event.offsetY);
     if(clickCount >= 2) {
-        showUserMessage("You have clicked too many times and have been reported to the FBI."); // thank you GitHub Copilot for this incredible autocompletion
+        showUserMessage("You have already clicked the maximum # of times.");
     } else {
         clickCount++;
         data.push([event.offsetX, event.offsetY]);
@@ -85,36 +119,7 @@ function clickHandler(event) {
                 setImageVisibility("point_2", true);
                 document.getElementById("point_2").style.left = event.offsetX + "px";
                 document.getElementById("point_2").style.top = event.offsetY + "px";
-
-                setImageVisibility("line_1", true);
-                setImageVisibility("line_2", true);
-                setImageVisibility("line_3", true);
-                setImageVisibility("line_4", true);
-
-                let x1 = data[0][0];
-                let y1 = data[0][1];
-                let x2 = data[1][0];
-                let y2 = data[1][1];
-
-                document.getElementById("line_1").style.left = x1 + "px";
-                document.getElementById("line_1").style.top = y1 + "px";
-                document.getElementById("line_1").style.width = Math.abs(x2 - x1) + "px";
-                document.getElementById("line_1").style.transform = "rotate(0deg)";
-
-                document.getElementById("line_2").style.left = (x1 + 8) + "px";
-                document.getElementById("line_2").style.top = y1 + "px";
-                document.getElementById("line_2").style.width = Math.abs(y1 - y2) + "px";
-                document.getElementById("line_2").style.transform = "rotate(90deg)";
-
-                document.getElementById("line_3").style.left = x2 + "px";
-                document.getElementById("line_3").style.top = y2 + "px";
-                document.getElementById("line_3").style.width = Math.abs(x2 - x1) + "px";
-                document.getElementById("line_3").style.transform = "rotate(180deg)";
-
-                document.getElementById("line_4").style.left = x2 + "px";
-                document.getElementById("line_4").style.top = y2 + "px";
-                document.getElementById("line_4").style.width = Math.abs(y1 - y2) + "px";
-                document.getElementById("line_4").style.transform = "rotate(-90deg)";
+                boundingBoxDrawer(data[0][0], data[0][1], data[1][0], data[1][1]);
 
                 break;
         }
@@ -138,4 +143,40 @@ function reset() {
     clickCount = 0;
     data = [];
     showUserMessage("You have clicked 0 times.");
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    let evtData = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(evtData));
+
+    console.log(event.x + ", " + event.y);
+
+    console.log(evtData);
+
+    let wrapperPos = document.getElementById("overlay_wrapper").getBoundingClientRect();
+    let new_loc = [event.x + window.scrollX - (wrapperPos.left + window.scrollX), event.y + window.scrollY - (wrapperPos.top + window.scrollY)];
+
+    let elem = document.getElementById(evtData);
+    elem.style.left = new_loc[0] + "px";
+    elem.style.top = new_loc[1] + "px";
+    switch(evtData) {
+        case "point_1":
+            data[0][0] = new_loc[0];
+            data[0][1] = new_loc[1];
+            break;
+        case "point_2":
+            data[1][0] = new_loc[0];
+            data[1][1] = new_loc[1];
+            break;
+    }
+    boundingBoxDrawer(data[0][0], data[0][1], data[1][0], data[1][1]);
 }
