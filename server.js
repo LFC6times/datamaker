4f28397659f7ac7ecd8d3426a611551b8f01b2a5
LFC6times <72238524+LFC6times@users.noreply.github.com>
2022-12-16 16:25:21 +0000

    commented code, added some sanity checks despite not haiving seen this for 2 months

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
+const TESTING = false; // set to false before actual use
+const PORT = 80; // HTTP, but means you'll need to use `sudo` to run the server on Linux
+
+const IMAGE_FOLDER_NAME = "./images";
+const DATA_OUTPUT_FOLDER_NAME = "./data";
+
+// Kinda config stuff above
+
 const fs = require("fs");
 const http = require("http");
 
+// Maps filename -> data point info
 let processedData = new Map();
+// Filenames of sent images for which a response has not been received
 let sent = [];
+// Filenames of images that still need to be sent
 let toSend = [];
 
-let htmlFile = fs.readFileSync("./app.html", "utf8");
-let jsFile = fs.readFileSync("./app.js", "utf8");
+const htmlFile = fs.readFileSync("./app.html", "utf8");
+const jsFile = fs.readFileSync("./app.js", "utf8");
+
+if(!fs.existsSync(IMAGE_FOLDER_NAME)) {
+    console.log("no image input folder, making (you need to put images in manually)");
+    try {
+        fs.mkdirSync(dir, {recursive: true});
+    } catch(e) {
+        console.log("Failed to create image input folder, exiting (try doing it yourself)");
+        process.exit(1);
+    }
+}
+if(!fs.existsSync(DATA_OUTPUT_FOLDER_NAME)) {
+    console.log("no data output folder, making");
+    try {
+        fs.mkdirSync(dir, {recursive: true});
+    } catch(e) {
+        console.log("Failed to create data output folder, exiting (try doing it yourself)");
+        process.exit(1);
+    }
+}
 
 fs.readdirSync("./images").forEach((file) => {
     if(!file.endsWith(".ini")) { // ignore ini files because they're weird (thanks windows)
@@ -27,15 +57,23 @@ fs.readdirSync("./data").forEach((file) => {
 console.log("data to send: " + toSend);
 
 function saveData() {
-    fs.writeFileSync(`./data/data${nextFileVal++}.json`, JSON.stringify(Array.from(processedData.entries())));
-    let writtenData;
-    processedData.forEach((value, key) => {
-        writtenData += key + "\n";
-    });
-    fs.writeFileSync("sorted.txt", writtenData); // The usage of this file is left as an exercise to the user.
+    let writtenData = "";
+    try {
+        fs.writeFileSync(`./data/data${nextFileVal++}.json`, JSON.stringify(Array.from(processedData.entries())));
+        processedData.forEach((value, key) => {
+            writtenData += key + "\n"; // shouldn't I like just write the keys and values together? TODO: rewrite later when I look at what 2 months ago me was thinking
+        });
+        fs.writeFileSync("sorted.txt", writtenData); // I don't exactly remember what I was thinking when I did this
+    } catch(e) {
+        console.log("failed to save data, e:")
+        console.log("err: " + e);
+        console.log("here's the data if you want to copy-paste it somewhere/debug: ");
+        console.log(writtenData); // should never happen
+    }
 }
 
-process.on("SIGINT", function () {
+// Allow server to save before closing
+process.on("SIGINT", () => {
     saveData();
     process.exit();
 });
@@ -63,7 +101,9 @@ const server = http.createServer((req, res) => {
                 res.end();
 
                 sent.push(send);
-                toSend.pop(); // Disable when testing, so that you can keep sending images without having to restart the server
+                if(!TESTING) {
+                    toSend.pop(); // Disable when testing, so that you can keep sending images without having to restart the server
+                }
             } else if(req.url === "/") {
                 res.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"});
                 res.write(htmlFile);
@@ -97,7 +137,7 @@ const server = http.createServer((req, res) => {
 });
 
 try {
-    server.listen(80); // HTTP port
+    server.listen(PORT);
 } catch (e) {
     saveData();
     console.log("error: " + e);
